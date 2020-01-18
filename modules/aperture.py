921ded862c1087374c8cd7521642f486d4146bca
Tajiri Tomoyuki <tomoyuki-tajiri@eps.s.u-tokyo.ac.jp>
2020-01-18 16:06:48 +0900

    translate in English

--- modules/aperture.py
+++ modules/aperture.py
@@ -15,45 +15,60 @@ import matplotlib.pyplot as plt
 
 def trim_aperture(img, sigma, mid_val, Q_std, area_thresh):
     #しきい値を決める
+    # determine threshold
     thresh = mid_val + sigma * Q_std
     #しきい値以下のものを0、他を1にする
+    # set the value below the threshold to 0 and the others to 1
     thimg = np.where(img > thresh, 1, 0).astype(np.uint8)
     #特徴検出
+    # find contours
     contours = cv2.findContours(thimg, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[0]
     #領域が大きすぎるものは排除
+    # remove large contour
     contours = [contour for contour in contours if cv2.contourArea(contour) <= area_thresh]
     #領域が小さすぎるものは排除
+    # remove small contour
     contours = [contour for contour in contours if contour.shape[0] >= 4]
     return contours
 
 def has_nearby_star(img, aperture):
     height, width = img.shape
     #画像中心
+    # center of image
     center = ((height - 1) // 2, (height - 1) // 2)
     center_flux = img[center[0], center[1]]
     #画像中心のみのaperture
+    # the aperture where only a center pixel has value 1
     center_aperture = np.zeros_like(img).astype(np.uint8)
     center_aperture[center[0], center[1]] = 1
     #中心近傍pixel
+    # a centeral pixel and neibourhood have value 1
     inner_area = cv2.dilate(center_aperture, np.ones((3, 3), np.uint8), iterations=1)
     #中心以外の近傍pixel
+    # center neibourhood have value 1
     neaby_area = inner_area - center_aperture
     #中心1pixel付近のaperture
+    # the aperture near the centeral pixel
     inner_aperture = np.where(np.logical_and(neaby_area, aperture), img ,np.nan)
     #中心1pixel外のaperture
+    # the aperture separated 1 or more pixel from the central pixel
     outer_aperture = np.where(np.logical_and(np.logical_not(inner_area), aperture), img ,np.nan)
     #中心1pixel付近のaperture内の最大flux
+    # the max flux in the aperture near the central pixel
     max_flux_inner = np.nanmax(inner_aperture)
     #中心1pixel外のaperture内の最大flux
+    # the max flux in the aperture separated 1 or more pixels from the central pixel
     max_flux_outer = np.nanmax(outer_aperture)
     #max_flux_outerがnanのときは無条件で通す
+    # if max_flux_outer is nan, the star is not contaminated
     if np.isnan(max_flux_outer):
         return False
     #中心点より高いfluxを持つpixelが、そのaperture内の中心pixel周辺外に存在する場合、近傍星は存在するとする
+    # If a pixel with flux higher than the center point exists outside the periphery of the center pixel in that aperture, the star is contaminated
     elif center_flux < max_flux_outer:
         return True
     #apertureに含まれる中心点近傍1pixelの最大fluxが、近傍1pixel外のapertureに含まれる星の最大fluxより小さい場合、近傍星は存在するとする
-    elif max_flux_inner < max_flux_outer:
+    # If the maximum flux of 1 pixel near the center point included in the aperture is smaller than the maximum flux of the stars included in the aperture outside the 1 pixel neighborhood, the star is contaminated
         return True
     else:
         return False
@@ -62,29 +77,38 @@ def determin_aperture(img, center, area_thresh=9):
     mid_val = np.nanmedian(img)
     img = np.nan_to_num(img)
     #統計量を求める。
+    # calculate statics
     flat_img = np.ravel(img)
     Q1 = stats.scoreatpercentile(flat_img, 25)
     Q3 = stats.scoreatpercentile(flat_img, 75)
     Q_std = Q3 - Q1
     #星中心を算出
+    # calculate the center of the star
     center_tuple = tuple(np.round(center).astype(np.uint8))
     #3Qstd以上の切り出し領域を求める
+    # calculate the cut area whose flux is larger than 3 Qstd
     contours = trim_aperture(img, 3, mid_val, Q_std, area_thresh)
     #4Qstd以上の切り出し領域を求める
+    # calculate the cut area whose flux is larger than 4 Qstd
     contours.extend(trim_aperture(img, 4, mid_val, Q_std, area_thresh))
     for contour in contours:
         #中心が含まれているか確認
+        # check whether the contour contains the central pixel
         has_center = cv2.pointPolygonTest(contour, center_tuple, False)
         if has_center >= 0:
             #apertureを作成
+            # make aperture
             aperture = np.zeros_like(img).astype(np.uint8)
             cv2.fillConvexPoly(aperture, points=contour, color=1)
             #近傍星がないか確認
+            # check whether the aperture is contaminated
             if not has_nearby_star(img, aperture):
                 break
     #決めかねてしまう場合
+    # if aperture cannot be determined by above process
     else:
         #中心含む4pixをapertureにする
+        # aperture is nearest 4 pixels from the center of the star
         offset = np.array([[0.5, 0.5], [0.5, -0.5], [-0.5, 0.5], [-0.5, -0.5]])
         aperture_contour = np.round(center + offset).astype(np.int32)
         aperture = np.zeros_like(img).astype(np.uint8)
--- modules/io.py
+++ modules/io.py
@@ -5,12 +5,21 @@ from astropy.io import fits
 from astropy.coordinates import SkyCoord
 from astropy.wcs import WCS, WCSSUB_LONGITUDE, WCSSUB_LATITUDE, WCSSUB_CELESTIAL
 
+"""
+All modules related to file I/O are listed in this file
+Import it and call function when you process IO
+"""
+
+
 datadir_org = "/%s/tess/data/FFI"
 workdir_org = "/%s/pipeline"
 dldir = "/var/tess/dl"
 
 
 def get_datadir(sector, filetype="ffic"):
+    """
+    get the directory which stores full frame images by sector
+    """
     if filetype == "ffic":
         if sector <= 11:
             datadir = datadir_org % "shishamo1"
@@ -18,31 +27,10 @@ def get_datadir(sector, filetype="ffic"):
             datadir = datadir_org % "shishamo2"
     return datadir
 
-def glob_fits(sector, camera, chip, sort=True):
-    if isinstance(sector, str):
-        sector = int(sector)
-    datadir = get_datadir(sector)
-    fitslist = glob.glob(os.path.join(datadir, "*s%04d-%s-%s*.fits" % (sector, camera, chip)))
-    if sort:
-        fitslist.sort()
-    return fitslist
-
-def glob_h5(data_type, sector, camera, chip, step):
-    h5dir = get_workdir(data_type, sector, step)
-    h5list = glob.glob(os.path.join(h5dir, "tess_*_%s_%s_%s.h5" % (sector, camera, chip)))
-    return h5list
-
-def load_wcs(fitslist):
-    for fitspath in fitslist:
-        hdu = fits.open(fitspath)
-        wcs = WCS(hdu[1].header)
-        bounds = hdu[1].data.shape
-        hdu.close()
-        if wcs.sub([WCSSUB_LONGITUDE, WCSSUB_LATITUDE]).naxis == 2:
-            break
-    return wcs, bounds
-
 def get_workdir(data_type, sector, step):
+    """
+    get the directory which stores intermediate files or light curve files by sector
+    """
     if data_type == "CTL":
         dir_name = "CTL%s" % step
     elif data_type == "TIC":
@@ -54,9 +42,43 @@ def get_workdir(data_type, sector, step):
     workdir = os.path.join(workdir_root, dir_name)
     return workdir
 
+def glob_fits(sector, camera, chip, sort=True):
+    """
+    get a fits list of full frame image by sector, camera and chip
+    """
+    if isinstance(sector, str):
+        sector = int(sector)
+    datadir = get_datadir(sector)
+    fitslist = glob.glob(os.path.join(datadir, "*s%04d-%s-%s*.fits" % (sector, camera, chip)))
+    if sort:
+        fitslist.sort()
+    return fitslist
+
+def glob_h5(data_type, sector, camera, chip, step):
+    """
+    get a hdf list of intermediate files or light curve files by sector, camera and chip
+    """
+    h5dir = get_workdir(data_type, sector, step)
+    h5list = glob.glob(os.path.join(h5dir, "tess_*_%s_%s_%s.h5" % (sector, camera, chip)))
+    return h5list
+
+def load_wcs(fitslist):
+    """
+    load WCS data from fits file
+    """
+    for fitspath in fitslist:
+        hdu = fits.open(fitspath)
+        wcs = WCS(hdu[1].header)
+        bounds = hdu[1].data.shape
+        hdu.close()
+        if wcs.sub([WCSSUB_LONGITUDE, WCSSUB_LATITUDE]).naxis == 2:
+            break
+    return wcs, bounds
+
 def get_target_path(data_type, TID, sector, camera, chip, step):
     """
     h5を出力するディレクトリパスを出力
+    get absolute path of a output file
     """
     outputdir = get_workdir(data_type, sector, step)
     tpfname = "tess_%s_%s_%s_%s.h5" % (TID, sector, camera, chip)
