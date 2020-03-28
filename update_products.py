11f28a978feaf285e64fab30fb90b64e1deddd82
Hajime Kawahara <divrot@gmail.com>
2020-03-28 21:15:13 +0900

    both directory

--- update_products.py
+++ update_products.py
@@ -4,31 +4,24 @@ import pathlib
 import time
 import datetime
 import numpy as np
-pathd="/manta/pipeline/CTL2"
-#pathd="/home/kawahara/tesstokyo/data"
-#l=glob.glob(pathd)
-
+pathdlist=["/manta/pipeline/CTL2","/stingray/pipeline/CTL2"]
 start = time.time()
-p=pathlib.Path(pathd)
-l=p.iterdir()
-print("Generating a file list completed.")
-f=open("h5.dat","a")
-i=0
-for ll in l:
-    if np.mod(i,100)==0:
-        print(i)
-    st=ll.stat()
-    name=ll.name
-    m=(datetime.datetime.fromtimestamp(st.st_mtime))
-    ns=name.split("_")
-    try:
-        tic=ns[1]
-        sector=ns[2]
-        line=(tic+","+sector+","+name+","+str(ll.parent)+","+str(m)+"\n")
-        f.write(line)
-    except:
-        print("Invalid type ",name)
-    i=i+1
+f=open("h5.dat","w")
+for pathd in pathdlist:
+    p=pathlib.Path(pathd)
+    l=p.iterdir()
+    for ll in l:
+        st=ll.stat()
+        name=ll.name
+        m=(datetime.datetime.fromtimestamp(st.st_mtime))
+        ns=name.split("_")
+        try:
+            tic=ns[1]
+            sector=ns[2]
+            line=(tic+","+sector+","+name+","+str(ll.parent)+","+str(m)+"\n")
+            f.write(line)
+        except:
+            print("Invalid type ",name)
 f.close()
 end = time.time()
 print(end-start,"sec")
